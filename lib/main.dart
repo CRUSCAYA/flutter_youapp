a5488c3f5ed9f1d0c2672561f05f96193993657e
cruscaya <ronaldo12roni@gmail.com>
2023-11-10 20:15:07 -0500

    Desarrollo de Init Page

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "YouApp",
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false, 
       home: InitPage(),
     );
   }
